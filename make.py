8c17a3ab9ef27697743501bf3a26ce2bda341c14
Paulo Coutinho <paulo@prsolucoes.com>
2019-11-18 23:22:51 -0300

    updated all to latest pdfium

--- make.py
+++ make.py
@@ -363,6 +363,8 @@ def run_task_install_ios(targets, target_configurations):
 
             files_str = " ".join(files)
 
+            print(files_str)
+
             lib_file_out = os.path.join(
                 "build", "ios", config, "libpdfium_{0}.a".format(target["target_cpu"])
             )
