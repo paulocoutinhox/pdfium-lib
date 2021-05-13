b55744883902ce28dae3b8490ce04002680a8d73
Mac Mini <paulo@ubook.com>
2021-05-13 00:13:11 -0300

    updated to 4505

--- modules/config.py
+++ modules/config.py
@@ -5,8 +5,8 @@ make_task = ""
 targets = ["ios", "macos", "android"]
 
 # pdfium
-pdfium_git_branch = "4493"
-pdfium_git_commit = "f115a5253d0699082058a1586d6b63fa998ba39e"
+pdfium_git_branch = "4505"
+pdfium_git_commit = "72fd656fee19235d9445796edee1e2c0c1e5e395"
 # ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/4493
 # OBS 1: don't forget change in android docker file (docker/android/Dockerfile)
 # OBS 2: don't forget change in wasm docker file (docker/wasm/Dockerfile)
