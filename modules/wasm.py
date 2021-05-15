8c0a1c73ba9e0cff07529785feb47f4d99aa4818
Paulo Coutinho <paulo@prsolucoes.com>
2021-05-15 08:39:06 -0300

    fix libjpeg wasm

--- modules/wasm.py
+++ modules/wasm.py
@@ -344,9 +344,7 @@ def run_task_patch():
     f.copy_file("/usr/include/zlib.h", os.path.join(source_dir, "zlib.h"))
     f.copy_file("/usr/include/zconf.h", os.path.join(source_dir, "zconf.h"))
     f.copy_file("/usr/include/jerror.h", os.path.join(source_dir, "jerror.h"))
-    f.copy_file(
-        "/usr/include/x86_64-linux-gnu/jconfig.h", os.path.join(source_dir, "jconfig.h")
-    )
+    f.copy_file("/usr/include/jconfig.h", os.path.join(source_dir, "jconfig.h"))
     f.copy_file("/usr/include/linux/limits.h", os.path.join(linux_dir, "limits.h"))
 
     f.debug("Copied!")
@@ -689,7 +687,7 @@ def run_task_generate():
                     "-s",
                     'EXPORTED_FUNCTIONS="$(node function-names ../xml/index.xml)"',
                     "-s",
-                    'EXTRA_EXPORTED_RUNTIME_METHODS=\'["ccall", "cwrap"]\'',
+                    'EXPORTED_RUNTIME_METHODS=\'["ccall", "cwrap"]\'',
                     "custom.cpp",
                     "@pdfium.rsp",
                     "-std=c++11",
