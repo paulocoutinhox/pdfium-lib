e31293ef2b08cf9cc361b2e31d09c7b9a925dc63
Paulo Coutinho <paulo@ubook.com>
2022-03-11 19:53:00 -0300

    updated to version 4938

--- modules/config.py
+++ modules/config.py
@@ -3,9 +3,9 @@ debug = False
 task = ""
 
 # pdfium
-pdfium_git_branch = "4795"
-pdfium_git_commit = "187d077faebb81a2bbc11d777ece46830cf5cd8f"
-# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/4795
+pdfium_git_branch = "4938"
+pdfium_git_commit = "626a3abbc9d53135270afbc2f4e9b5de2b5c59ce"
+# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/4938
 # OBS 1: don't forget change in android docker file (docker/android/Dockerfile)
 # OBS 2: don't forget change in wasm docker file (docker/wasm/Dockerfile)
 
