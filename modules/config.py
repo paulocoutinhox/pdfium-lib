f4147553a80e66de4a438ee77849112174b49d7e
“Paulo <paulo@audimo.com>
2023-08-25 02:14:24 -0300

    update pdfium version to 5968

--- modules/config.py
+++ modules/config.py
@@ -3,14 +3,14 @@ debug = False
 task = ""
 
 # pdfium
-pdfium_git_branch = "5668"
-pdfium_git_commit = "15b1203da8e9be060d143a184de6473ca7a68b83"
-# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/5668
+pdfium_git_branch = "5968"
+pdfium_git_commit = "7952b46ddd1db647dc618b5caf890706ec3bd6d7"
+# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/5968
 # OBS 1: don't forget change in android docker file (docker/android/Dockerfile)
 # OBS 2: don't forget change in wasm docker file (docker/wasm/Dockerfile)
 
 # emsdk
-emsdk_version = "3.1.34"
+emsdk_version = "3.1.45"
 # OBS 1: don't forget change in wasm docker file (docker/wasm/Dockerfile)
 
 # macos
