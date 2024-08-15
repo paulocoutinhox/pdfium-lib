24e90951089ffeb9a3e8030ea6f4dc38c7349d20
“Paulo <paulo@ubook.com>
2024-08-15 10:40:49 -0300

    updated to chromium/6658

--- modules/config.py
+++ modules/config.py
@@ -3,13 +3,13 @@ debug = False
 task = ""
 
 # pdfium
-pdfium_git_branch = "chromium/6276"
-# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/6276
+pdfium_git_branch = "chromium/6658"
+# ^ ref: https://pdfium.googlesource.com/pdfium/+/refs/heads/chromium/6658
 # OBS 1: don't forget change in android docker file (docker/android/Dockerfile)
 # OBS 2: don't forget change in wasm docker file (docker/wasm/Dockerfile)
 
 # emsdk
-emsdk_version = "3.1.50"
+emsdk_version = "3.1.64"
 # OBS 1: don't forget change in wasm docker file (docker/wasm/Dockerfile)
 
 # macos
